59a84f36bebb262ad3b460826997107d47740e90
thiago-rferreira <thiagoferreira435@gmail.com>
2025-10-20 09:43:52 -0300

    getbyid

--- src/models/bruxoModel.js
+++ src/models/bruxoModel.js
@@ -13,3 +13,11 @@ export const findAll = async () => {
         orderBy: { nome: 'asc' }
     });
 }
+
+// Crio a variavel findById e já exporto
+export const findById = async (id) => {
+    // SELECT * FROM bruxos WHERE id = 1;
+    return await prisma.bruxo.findUnique({
+        where: { id: Number(id) }
+    });
+}
--- src/routes/bruxoRoute.js
+++ src/routes/bruxoRoute.js
@@ -5,9 +5,7 @@ const router = Router();
 
 //Rota GetAll em /
 router.get("/", BruxoController.listarTodos);
-
-
-
-
+//Rota GetById
+router.get("/:id", BruxoController.listarUm)
 
 export default router;
